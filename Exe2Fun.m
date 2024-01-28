a9466afa857ee170fc7b3bba1050b808e88f0cb7
johnsiak <149271195+johnsiak@users.noreply.github.com>
2024-01-28 20:29:14 +0200

    Update code

--- Exe2Fun.m
+++ Exe2Fun.m
@@ -1,6 +1,3 @@
-%Ioannis Siakavaras
-%Christoforos Chatziantoniou
-
 function [mean_par, mean_boot] = Group30Exe2Fun(sampleData)
     [~, ~, mean_par] = ttest(sampleData); 
     mean_boot = bootci(2500, @mean, sampleData);   
